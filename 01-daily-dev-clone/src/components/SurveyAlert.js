b12e0435610bdb1620bd49d27330103d86e543ee
heyfaraaiden <heyfaraaiden@gmail.com>
2021-12-24 07:53:09 +0700

    Fix some problems: button icons on header too small on mobile & need more space

--- 01-daily-dev-clone/src/components/SurveyAlert.js
+++ 01-daily-dev-clone/src/components/SurveyAlert.js
@@ -9,7 +9,7 @@ const SurveyAlert = () => {
     <div className='survey-alert'>
       <div className='container survey-alert__container'>
         <p className='survey-alert__text'>
-          <span className='strong'>Shape the future of daily.dev!</span> 1,000 free limited-edition stickers inside 🎁
+          <span className='strong'>Shape the future of daily.dev!</span> 1,000 free limited-edition stickers&nbsp;inside&nbsp;🎁
         </p>
         <a className='survey-alert__cta' href='https://daily.dev'>
           Start the survey
